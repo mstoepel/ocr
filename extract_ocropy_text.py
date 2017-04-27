d485b1c50b122d6a67f6b400522544f589b6a7f1
Michael Stoepel <mstoepel@gmail.com>
2017-04-26 21:30:18 -0400

    created dataframe to store values

--- extract_ocropy_text.py
+++ extract_ocropy_text.py
@@ -10,8 +10,7 @@ Output is text, printed to stdout.
 
 import re
 import sys
-
-
+import pandas as pd
 from bs4 import BeautifulSoup
 
 
@@ -57,6 +56,8 @@ def sort_lines(lines):
 def hocr_to_lines(hocr_path):
     lines = []
     soup = BeautifulSoup(file(hocr_path))
+    dims = re.search(r'(-?\d{4}) (-?\d{4})',soup.select('.ocr_page')[0].get('title'))
+    width, length = (int(v) for v in dims.groups())
     for tag in soup.select('.ocr_line'):
         m = re.match(r'bbox (-?\d+) (-?\d+) (-?\d+) (-?\d+)', tag.get('title'))
         assert m
@@ -75,28 +76,54 @@ def hocr_to_lines(hocr_path):
     for line in lines:
         line['y1'] =  line['y1']
         line['y2'] =  line['y2']
-    return lines
+    return lines, width, length
 
 
 if __name__ == '__main__':
     _, hocr_path = sys.argv
     f = open('ocr_output.txt','w')
-    lines = hocr_to_lines(hocr_path)
+    lines,width,length = hocr_to_lines(hocr_path)
     lines = sort_lines(lines)
-    print lines
-
+    df = pd.DataFrame(index=[i for i in range(14)],columns=['Owner_1','Owner_2','Location','Description','Construction Value','Permit Fee'])
+    #print lines
     output = ''
     for idx, line in enumerate(lines):
-        if idx > 0:
-            if line.get('continuation'):
-                output += '  '
-            else:
-                output += '\n'
+        # if idx > 0:
+            # if line.get('continuation'):
+            #     output += '  '
+            # else:
+            #     output += '\n'
+        if line.get('remove'):
+            lines.pop(idx)
+            idx =- idx
+            break
+        if lines[idx+1].get('continuation'):
+            lines[idx] = lines[idx]['text'] + " " + lines[idx+1]['text']
+        if line.get('continuation'):
+            lines.pop(idx)
+            idx =- idx
+            break
         if 'text' in line and not line.get('remove'):
             output += line['text']
-        # if line.get('remove'):
-        #     del line
+            for i in range(84):
+                if i in (1,3,5,7,9,11,13,15,17,19,21,23,25,27):
+                    df.set_value((i-1)//2,'Owner_1',lines[i-1]['text'])
+                elif i in (2,4,6,8,10,12,14,16,18,20,22,24,26,28):
+                    df.set_value((i-1)//2,'Owner_2',lines[i-1]['text'])
+                elif i in (29,30,31,32,33,34,35,36,37,38,39,40,41,42):
+                    df.set_value((i%28)-1,'Location',lines[i]['text'])
+                elif i in (43,44,45,46,47,48,49,50,51,52,53,54,55,56):
+                    df.set_value((i%42)-1,'Description',lines[i]['text'])
+                elif i in (57,58,59,60,61,62,63,64,65,66,67,68,69,70):
+                    df.set_value((i%56)-1,'Construction Value',lines[i]['text'])
+                elif i in (71,72,73,74,75,76,77,78,79,80,81,82,83,84):
+                    df.set_value((i%70)-1,'Permit Fee',lines[i]['text'])
     f.write(output.encode('utf8'))
     f.close()
-    # print output
+    print df
+    # print lines[0]
+    # for idx, line in enumerate(lines):
+    #     if line.get('remove'):
+    #         lines.pop(idx)
+    # print lines
 
